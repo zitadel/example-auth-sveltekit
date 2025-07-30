633cd6642b7fb368d903285c85098eb0737faaa6
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-07-30 12:20:23 +0530

    chore(dev): streamlined the prettier config and reformatted the codebase

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,8 +1,18 @@
 export default {
-	semi: true,
-	trailingComma: 'all',
-	singleQuote: true,
-	printWidth: 80,
-	tabWidth: 2,
-	useTabs: false
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  plugins: ['prettier-plugin-svelte', 'prettier-plugin-tailwindcss'],
+  overrides: [
+    {
+      files: '*.svelte',
+      options: {
+        parser: 'svelte',
+      },
+    },
+  ],
+  tailwindStylesheet: './src/app.css',
 };
