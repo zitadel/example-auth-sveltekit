972c7c9eec6f3e1f0ec5f028ae2052b0a1fe9574
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2026-04-11 10:08:16 +0700

    chore: add commitlint configuration

--- knip.config.js
+++ knip.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  ignore: ['commitlint.config.js'],
+  ignoreDependencies: ['@commitlint/config-conventional'],
   paths: {
     '$env/static/private': ['.svelte-kit/ambient.d.ts'],
   },
